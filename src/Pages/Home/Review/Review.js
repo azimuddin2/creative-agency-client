5463fd3fb7a573027247953cda0a1a884b223205
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-11-25 09:31:22 +0600

    updated is review component

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './Review.css';
+import comma from '../../../assets/images/comma.png';
 
-const Review = ({review}) => {
-    const {img, name, position, description} = review;
+const Review = ({ review }) => {
+    const { img, name, position, description } = review;
 
     return (
         <div className='review'>
             <div className='review-info'>
-                <div className='review-image'>
-                    <img src={img} alt={name} />
+                <div className='review-info-part'>
+                    <div className='review-image'>
+                        <img src={img} alt={name} />
+                    </div>
+                    <div className='client-title'>
+                        <h5>{name}</h5>
+                        <h6>{position}</h6>
+                    </div>
                 </div>
-                <div className='client-title'>
-                    <h5>{name}</h5>
-                    <h6>{position}</h6>
+                <div className='comma'>
+                    <img src={comma} alt="" />
                 </div>
             </div>
             <div className='review-description'>
